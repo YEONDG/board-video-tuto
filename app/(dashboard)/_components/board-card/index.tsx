85f8a693350e86ba32bf2029709c66637f5ea0da
YDG <zzmn1234@naver.com>
2024-04-23 10:05:59 +0900

    Favoriting functionality

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -1,16 +1,19 @@
 'use client';
 
-import { FormatDateOptions, formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow } from 'date-fns';
 import Image from 'next/image';
 import Link from 'next/link';
-import { auth, useAuth } from '@clerk/nextjs';
-
-import { Overlay } from './overlay';
-import { Footer } from './footer';
-import { Actions } from '@/components/actions';
-
-import { Skeleton } from '@/components/ui/skeleton';
+import { useAuth } from '@clerk/nextjs';
 import { MoreHorizontal } from 'lucide-react';
+
+import { Actions } from '@/components/actions';
+import { Skeleton } from '@/components/ui/skeleton';
+import { useApiMutation } from '@/hooks/use-api-mutatation';
+import { api } from '@/convex/_generated/api';
+
+import { Footer } from './footer';
+import { Overlay } from './overlay';
+import { toast } from 'sonner';
 
 interface BoardCardProps {
   id: string;
@@ -38,6 +41,21 @@ export const BoardCard = ({
   const authorLabel = userId === authorId ? 'You' : authorName;
   const createdAtLabel = formatDistanceToNow(createdAt, { addSuffix: true });
 
+  const { mutate: onFavorite, pending: pendingFavorite } = useApiMutation(
+    api.board.favorite
+  );
+  const { mutate: onUnFavorite, pending: pendingUnFavorite } = useApiMutation(
+    api.board.unfavorite
+  );
+
+  const toggleFavorite = () => {
+    if (isFavorite) {
+      onUnFavorite({ id }).catch(() => toast.error('Failed to unfavorite'));
+    } else {
+      onFavorite({ id, orgId }).catch(() => toast.error('Failed to favorite'));
+    }
+  };
+
   return (
     <Link href={`/board/${id}`}>
       <div className='group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden'>
@@ -55,8 +73,8 @@ export const BoardCard = ({
           title={title}
           authorLabel={authorLabel}
           createdAtLabel={createdAtLabel}
-          onClick={() => {}}
-          disabled={false}
+          onClick={toggleFavorite}
+          disabled={pendingFavorite || pendingUnFavorite}
         />
       </div>
     </Link>
